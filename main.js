cd9a084c280522a0a15130e512a3085d2f974e95
Rafael Jürkfitz <rafaeljurkfitz@hotmail.com>
2022-01-27 21:02:13 -0300

    aula 2

---
+++ main.js
@@ -0,0 +1,19 @@
+const modal = {
+    open(){
+        //abrir o modal
+        //adicionar a class active ao modal
+        document
+                .querySelector('.modal-overlay')
+                .classList
+                .add('active')
+    },
+    close() {
+        //fechar o modal
+        //remover a class active do modal
+        document
+                .querySelector('.modal-overlay')
+                .classList
+                .remove('active')
+    }
+};
+
